d73e0a6ad73067d04b8b9d53cf27f09264e0971d
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-02-21 07:52:17 +0200

    Add PropTypes

--- src/components/Slider/Slide/index.jsx
+++ src/components/Slider/Slide/index.jsx
@@ -1,25 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
+function Slide(props) {
+  const { img, title, description, prevSlide, nextSlide } = props;
 
-function Slide (props) {
-  
-  const {img, title, description,prevSlide, nextSlide} = props;
-
-
-    return (
-      <>
-        <div>
-          <button onClick={prevSlide}>{"<"}</button>
-          <img src={img} alt={title} />
-          <button onClick={nextSlide}>{">"}</button>
-        </div>
-        <div>
-          <h1>{title}</h1>
-          <p>{description}</p>
-        </div>
-      </>
-    );
-  
+  return (
+    <>
+      <div>
+        <button onClick={prevSlide}>{"<"}</button>
+        <img src={img} alt={title} />
+        <button onClick={nextSlide}>{">"}</button>
+      </div>
+      <div>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </div>
+    </>
+  );
 }
+
+Slide.propTypes = {
+  img: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  prevSlide: PropTypes.func,
+  nextSlide: PropTypes.func,
+};
 
 export default Slide;
